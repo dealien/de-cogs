3c416f1191b1d317c4c7d7cbf7f33070808fbcf4
dealien <dealien@users.noreply.github.com>
2017-06-04 11:54:03 -0400

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-try: # check if BeautifulSoup4 is installed
+try: # Check if BeautifulSoup4 is installed
     from bs4 import BeautifulSoup
     soupAvailable = True
 except:
@@ -24,40 +24,48 @@ class Mycog:
     async def mycom(self):
         """This does stuff!"""
 
-        #Command function
+        # Command function
         await self.bot.say("I can do stuff!")
+
+    @commands.command()
+    async def punch(self, user : discord.Member):
+        """I will puch anyone! >.<"""
+
+        # Command function
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+
+    # @commands.command()
+    # async def gtakd(self):
+    #     """Check the kill-death ratio of a player in Grand Theft Auto: Online"""
+
+    #     # Command function
+    #     # Build the web address
+    #     url = "https://socialclub.rockstargames.com/member/--zephyr--/games/gtav/pc/career/stats/gtaonline/career"
+    #     async with aiohttp.get(url) as response:
+    #     soupObject = BeautifulSoup(await response.text(), "html.parser")
+    #     print soupObject
+    #     try:
+    #         online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+    #         await self.bot.say(online + ' players are playing this game at the moment')
+    #     except:
+    #         await self.bot.say("Couldn't load data.")
     
     @commands.command()
-    async def gtakd(self):
-        """Check the kill-death ratio of a player in Grand Theft Auto: Online"""
+    async def dotanow(self):
+        """How many players are online atm?"""
 
-        #Command function
-        url = "https://socialclub.rockstargames.com/member/--zephyr--/games/gtav/pc/career/stats/gtaonline/career" #build the web adress
+        # Your code will go here
+        url = "https://steamdb.info/app/570/graphs/" #build the web adress
         async with aiohttp.get(url) as response:
-        soupObject = BeautifulSoup(await response.text(), "html.parser")
-        print soupObject
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
         try:
             online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
             await self.bot.say(online + ' players are playing this game at the moment')
         except:
-            await self.bot.say("Couldn't load data.")
-            
+            await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+
+
 def setup(bot):
     bot.add_cog(Mycog(bot))
 
-    
-    
-    
-@commands.command()
-async def dotanow(self):
-    """How many players are online atm?"""
 
-    #Your code will go here
-    url = "https://steamdb.info/app/570/graphs/" #build the web adress
-    async with aiohttp.get(url) as response:
-        soupObject = BeautifulSoup(await response.text(), "html.parser")
-    try:
-        online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
-        await self.bot.say(online + ' players are playing this game at the moment')
-    except:
-        await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
