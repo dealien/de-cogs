ce5a38bb72e62ea5d6fb7df2a9654375a8c19209
dealien <dealien@users.noreply.github.com>
2017-06-04 11:01:52 -0400

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -35,20 +35,19 @@ class Mycog:
         await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
     
     @commands.command()
-async def gtakd(self):
-    """Check the kill-death ratio of a player in Grand Theft Auto: Online"""
+	async def gtakd(self):
+	    """Check the kill-death ratio of a player in Grand Theft Auto: Online"""
 
-    #Your code will go here
-    url = "https://socialclub.rockstargames.com/member/--zephyr--/games/gtav/pc/career/stats/gtaonline/career" #build the web adress
-    async with aiohttp.get(url) as response:
-        soupObject = BeautifulSoup(await response.text(), "html.parser")
-        print soupObject
-    try:
-        online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
-        await self.bot.say(online + ' players are playing this game at the moment')
-    except:
-        await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
-        
-        
+	    #Your code will go here
+	    url = "https://socialclub.rockstargames.com/member/--zephyr--/games/gtav/pc/career/stats/gtaonline/career" #build the web adress
+	    async with aiohttp.get(url) as response:
+		soupObject = BeautifulSoup(await response.text(), "html.parser")
+		print soupObject
+	    try:
+		online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+		await self.bot.say(online + ' players are playing this game at the moment')
+	    except:
+		await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+
 def setup(bot):
     bot.add_cog(Mycog(bot))
