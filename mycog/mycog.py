423fd15a2b65de0b07d739b738e4d2bea474f9ac
dealien <dealien@users.noreply.github.com>
2017-06-04 11:14:08 -0400

    Update mycog.py

--- mycog/mycog.py
+++ mycog/mycog.py
@@ -2,52 +2,52 @@ import discord
 from discord.ext import commands
 
 try: # check if BeautifulSoup4 is installed
-	from bs4 import BeautifulSoup
-	soupAvailable = True
+        from bs4 import BeautifulSoup
+        soupAvailable = True
 except:
-	soupAvailable = False
+        soupAvailable = False
 import aiohttp
 
 def setup(bot):
-	if soupAvailable:
-		bot.add_cog(Mycog(bot))
-	else:
-		raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
+        if soupAvailable:
+                bot.add_cog(Mycog(bot))
+        else:
+                raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
 
 class Mycog:
-    """My custom cog that does stuff!"""
+        """My custom cog that does stuff!"""
 
-    def __init__(self, bot):
-        self.bot = bot
+        def __init__(self, bot):
+                self.bot = bot
 
-    @commands.command()
-    async def mycom(self):
-        """This does stuff!"""
+        @commands.command()
+        async def mycom(self):
+                """This does stuff!"""
 
-        #Your code will go here
-        await self.bot.say("I can do stuff!")
+                #Command function
+                await self.bot.say("I can do stuff!")
     
-    @commands.command()
-    async def punch(self, user : discord.Member):
-        """I will puch anyone! >.<"""
+        @commands.command()
+        async def punch(self, user : discord.Member):
+                """I will puch anyone! >.<"""
 
-        #Your code will go here
-        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+                #Command function
+                await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
     
-    @commands.command()
+        @commands.command()
 	async def gtakd(self):
-	    """Check the kill-death ratio of a player in Grand Theft Auto: Online"""
+	        """Check the kill-death ratio of a player in Grand Theft Auto: Online"""
 
-	    #Your code will go here
-	    url = "https://socialclub.rockstargames.com/member/--zephyr--/games/gtav/pc/career/stats/gtaonline/career" #build the web adress
-	    async with aiohttp.get(url) as response:
-		soupObject = BeautifulSoup(await response.text(), "html.parser")
-		print soupObject
-	    try:
-		online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
-		await self.bot.say(online + ' players are playing this game at the moment')
-	    except:
-		await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+                #Command function
+                url = "https://socialclub.rockstargames.com/member/--zephyr--/games/gtav/pc/career/stats/gtaonline/career" #build the web adress
+                async with aiohttp.get(url) as response:
+		        soupObject = BeautifulSoup(await response.text(), "html.parser")
+		        print soupObject
+	        try:
+        		online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+	        	await self.bot.say(online + ' players are playing this game at the moment')
+                except:
+		        await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
